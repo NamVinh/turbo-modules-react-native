86edfdd46cba2d4023f2a876761a075aa4897e37
Nam Vinh <vinhdn@biso24.com>
2024-04-04 15:14:15 +0700

    init: add react native template

--- packages/tools/eslint-config-standard-react/index.js
+++ packages/tools/eslint-config-standard-react/index.js
@@ -1,59 +1,64 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  extends: ["standard-with-typescript", "prettier"],
-  plugins: ["react", "react-hooks", "prefer-arrow-functions", "autofix"],
-  rules: {
-    "autofix/arrow-body-style": ["error", "always"],
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: ['standard-with-typescript', 'prettier'],
+	plugins: ['react', 'react-hooks', 'prefer-arrow-functions', 'autofix'],
+	rules: {
+		// JS rules
+		'default-case': 'off',
+		curly: ['error', 'all'],
+		'no-case-declarations': 'error',
 
-    // TS rules
-    "@typescript-eslint/explicit-function-return-type": "off",
+		// TS
+		'@typescript-eslint/strict-null-checks': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
 
-    // eslint-plugin-autofix
-    "autofix/no-unused-vars": "error",
+		// eslint-plugin-autofix
+		'autofix/no-unused-vars': 'error',
+		'autofix/arrow-body-style': ['error', 'always'],
 
-    // eslint-plugin-prefer-arrow-functions
-    "prefer-arrow-functions/prefer-arrow-functions": [
-      "error",
-      {
-        classPropertiesAllowed: false,
-        disallowPrototype: false,
-        returnStyle: "unchanged",
-        singleReturnOnly: false,
-      },
-    ],
+		// eslint-plugin-prefer-arrow-functions
+		'prefer-arrow-functions/prefer-arrow-functions': [
+			'error',
+			{
+				classPropertiesAllowed: false,
+				disallowPrototype: false,
+				returnStyle: 'unchanged',
+				singleReturnOnly: false,
+			},
+		],
 
-    // eslint-plugin-react
-    "react/jsx-boolean-value": "error",
-    "react/jsx-curly-brace-presence": ["error", "never"],
-    "react/function-component-definition": [
-      "error",
-      {
-        namedComponents: "arrow-function",
-        unnamedComponents: "arrow-function",
-      },
-    ],
-    "react/self-closing-comp": [
-      "error",
-      {
-        html: true,
-        component: true,
-      },
-    ],
+		// eslint-plugin-react
+		'react/jsx-boolean-value': 'error',
+		'react/jsx-curly-brace-presence': ['error', 'never'],
+		'react/function-component-definition': [
+			'error',
+			{
+				namedComponents: 'arrow-function',
+				unnamedComponents: 'arrow-function',
+			},
+		],
+		'react/self-closing-comp': [
+			'error',
+			{
+				html: true,
+				component: true,
+			},
+		],
 
-    // eslint-plugin-react-hooks
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-  },
+		// eslint-plugin-react-hooks
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+	},
 };
